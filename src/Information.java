c0d6c00c51fbac704758d769a2fc88f44f0c017c
kfmrc0 <kkfong4516@gmail.com>
2025-09-03 21:09:57 -0700

    added some information

--- src/Information.java
+++ src/Information.java
@@ -1,7 +1,22 @@
 public class Information {
 
     public static void test() {
-        System.out.println("5. info received");
+        System.out.println("""
+                \n1. --- Intro
+                    This project was made by kfmrc0 for fun
+                    
+                2. --- Discounted Cash Flow
+                    The discounted cash flow model...
+                    
+                3. --- Weight Average Cost of Capital
+                    weighted
+                    
+                4. --- Earnings Multiplier
+                    pe ratio'
+                
+                5. --- Dividend Discount Model
+                    ddm
+                """);
     }
 
 }
