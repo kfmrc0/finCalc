f71f3821020a87194a11ca174b3cd4293ec15bf9
kfmrc0 <kkfong4516@gmail.com>
2025-09-04 21:53:43 -0700

    personal note and disclaimer

--- src/Information.java
+++ src/Information.java
@@ -2,8 +2,16 @@ public class Information {
 
     public static void test() {
         System.out.println("""
-                \n1. --- Intro
-                    This project was made by kfmrc0 for fun
+                \n1. --- Notes
+                    This project was made by kfmrc0 for fun and for personal use. eventually want
+                    to learn spring and spring boot for a web app with similar use or application
+                    of this program. any formulas or equations involved were found on the web. in
+                    my (Kfmrc0) opinion, this kind of info should be accessible and free without
+                    any annoying pop-ups, sign-ups, or paywalls. thanks
+                             
+                        NOTE: using this program is not a form of financial advice. use at your
+                        own risk. The user (YOU) holds all responsibilities and liabilities in
+                        their own financial decisions.
                     
                 2. --- Discounted Cash Flow
                     The discounted cash flow model...
