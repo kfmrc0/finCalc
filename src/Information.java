50de5e4a42621dcfa262299178221833ab512ad8
kfmrc0 <kkfong4516@gmail.com>
2025-09-13 19:24:20 -0700

    info

--- src/Information.java
+++ src/Information.java
@@ -34,7 +34,7 @@ public class Information {
                 4. --- Earnings Multiplier
                     price to earnings multiple, stock price and earnings per share. stock price / eps
                     eps * pe = intrinsic value
-                    yahoo finance
+                    yahoo finance for numbers
                 
                 5. --- Dividend Discount Model
                     ddm test testtest
