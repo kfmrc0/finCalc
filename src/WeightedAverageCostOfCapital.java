62ed7f468a35ab1447bc1e6c153cdd8a2a955e3e
kfmrc0 <kkfong4516@gmail.com>
2025-10-02 19:57:34 -0700

    completed output menu, maybe will change info

--- src/WeightedAverageCostOfCapital.java
+++ src/WeightedAverageCostOfCapital.java
@@ -29,24 +29,24 @@ public class WeightedAverageCostOfCapital {
                 \n--- WACC Calculation ---
                 Ticker: %s
                 
-                Cost of Debt:           %.2f
-                Effective Tax Rate:     %.2f
-                After-Tax Cost of Debt: %.2f
-                Cost of Equity:         %.2f
+                Cost of Debt:           %.2f%%
+                Effective Tax Rate:     %.2f%%
+                After-Tax Cost of Debt: %.2f%%
+                Cost of Equity:         %.2f%%
                 
-                Debt Weight:            %.2f
-                Equity Weight:          %.2f
+                Debt Weight:            %.2f%%
+                Equity Weight:          %.2f%%
                 
                 WACC:                   %.2f%%
                 -----------------------------
                 """,
                 ticker,
-                costOfDebt,
-                effectiveTaxRate,
-                costOfDebtAfterTax,
-                costOfEquity,
-                DEtotalDebt,
-                DEmarketCap,
+                costOfDebt * 100,
+                effectiveTaxRate * 100,
+                costOfDebtAfterTax * 100,
+                costOfEquity * 100,
+                DEtotalDebt * 100,
+                DEmarketCap * 100,
                 WACC * 100
         ); // show calculated variables too later
     }
